82f9be552ca19f463a48150609cb7b671a2dae1d
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-09-27 12:23:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -324,7 +324,6 @@ function base64ToArrayBuffer(base64Str) {
 		return { error: null };
 	}
 	try {
-		// go use modified Base64 for URL rfc4648 which js atob not support
 		base64Str = base64Str.replace(/-/g, '+').replace(/_/g, '/');
 		const decode = atob(base64Str);
 		const arryBuffer = Uint8Array.from(decode, (c) => c.charCodeAt(0));
