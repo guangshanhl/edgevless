a204dcff31c69393bf83a52dd6f556370f88d210
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-10-03 14:47:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -69,18 +69,16 @@ const connectAndWrite = async (remoteSocket, address, port, rawClientData) => {
   return socket;
 };
 const handleTcpRequest = async (remoteSocket, addressRemote, portRemote, rawClientData, webSocket, responseHeader, proxyIP) => {
-  let mainSocket, proxySocket;
+    let mainSocket, proxySocket;
     try {
-      mainSocket = await connectAndWrite(remoteSocket, addressRemote, portRemote, rawClientData);
-      proxySocket = await connectAndWrite(remoteSocket, proxyIP, portRemote, rawClientData);
-      await Promise.all([
-        await forwardToData(mainSocket, webSocket, responseHeader),
-        await forwardToData(proxySocket, webSocket, responseHeader)
-      ]);
-    } catch (error) {
-      closeWebSocket(webSocket);
+        mainSocket = await connectAndWrite(remoteSocket, addressRemote, portRemote, rawClientData);
+        await forwardToData(mainSocket, webSocket, responseHeader);
+        const proxySocket = await connectAndWrite(remoteSocket, proxyIP, portRemote, rawClientData);
+        await forwardToData(proxySocket, webSocket, responseHeader);
+    } catch (Error) {
+        closeWebSocket(webSocket);
     } finally {
-      rawClientData = null;
+        rawClientData = null;
     }
 };
 const eventHandlers = new WeakMap();
