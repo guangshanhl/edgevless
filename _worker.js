65f1841ce62d9d0ecde35a7b984e6183cdf484a0
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-10-05 15:28:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -412,6 +412,6 @@ async function handleUDPOutBound(webSocket, vlessResponseHeader, log) {
 		}
 	};
 }
-function getVLESSConfig(userID, hostName) {
-`vless://${userID}\u0040${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#${hostName}`;
-}
+const getVLESSConfig = (userID, hostName) => `
+vless://${userID}@${hostName}:8443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#${hostName}
+`;
