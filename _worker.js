6edb44e2fee066af443966353109acbc53fd6c53
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-10-08 12:50:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -73,16 +73,13 @@ const connectAndWrite = async (remoteSocket, address, port, rawClientData) => {
 };
 const handleTcpRequest = async (remoteSocket, addressRemote, portRemote, rawClientData, webSocket, responseHeader, proxyIP) => {
   const connectAndForward = async (address) => {
-  try {
-    const tcpSocket = await connectAndWrite(remoteSocket, address, portRemote, rawClientData);
-    await forwardToData(tcpSocket, webSocket, responseHeader);
-    return tcpSocket; // 返回 TCP socket
-  } catch (error) {
-    console.error(`Error connecting or forwarding to ${address}: ${error.message}`);
-    return null; // 连接或转发失败
-  }
-};
-
+    try {
+      const tcpSocket = await connectAndWrite(remoteSocket, address, portRemote, rawClientData);
+      await forwardToData(tcpSocket, webSocket, responseHeader);
+    } catch (error) {
+      return null;
+    }
+  };
   try {
     const primaryTcpSocket = await connectAndForward(addressRemote);
     if (primaryTcpSocket) {
