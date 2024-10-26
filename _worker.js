b497b20b3063a2abec83c64845544756110c5225
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-10-26 15:17:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -28,16 +28,9 @@ const handleHttpRequest = (request, userID) => {
     }
     return new Response("Not found", { status: 404 });
 };
-const wsCache = new Map();
 const handleWsRequest = async (request, userID, proxyIP) => {
-    let cachedSocket = wsCache.get(userID);
-    if (cachedSocket && cachedSocket.readyState === WebSocket.OPEN) {
-        return new Response(null, { status: 101, webSocket: cachedSocket.clientSocket });
-    }
     const [clientSocket, serverSocket] = new WebSocketPair();
     serverSocket.accept();
-    wsCache.set(userID, { clientSocket, serverSocket });
-    console.log(cachedSocket);
     const earlyDataHeader = request.headers.get('sec-websocket-protocol') || '';
     const readableStream = createWebSocketStream(serverSocket, earlyDataHeader, userID);
     let remoteSocket = { value: null };
