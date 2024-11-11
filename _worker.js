b63594c2c81653554e799d7cf03e936e7aed82ed
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-11-11 19:26:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -383,7 +383,7 @@ async function handleUDPOutBound(webSocket, vlessResponseHeader, log) {
 	};
 }
 function getVLESSConfig(userID, hostName) {
-	const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`
+	const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#${hostName}`
 	return `
 ################################################################
 v2ray
