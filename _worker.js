87fc54893664f2d9275a00ca7bd1fb174e0b8e14
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-11-12 22:44:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -73,7 +73,7 @@ const connectAndWrite = async (remoteSocket, address, port, clientData) => {
     await writeToRemote(remoteSocket.value, clientData);
     return remoteSocket.value;
   } else {
-    const tcpSocket = connect({ hostname: address, port });
+    const tcpSocket = connect({ hostname: address, port, signal: AbortSignal.timeout(2000) });
     remoteSocket.value = tcpSocket;
     await writeToRemote(tcpSocket, clientData);
     return tcpSocket;
