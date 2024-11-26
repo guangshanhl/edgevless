dbfe09553b56a9dd829a3f42b347e891fc542e6d
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-11-26 14:54:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -105,7 +105,7 @@ async function handleTCPOutBound(remoteSocket, addressRemote, portRemote, client
         const writer = remoteSocket.value.writable.getWriter();
         try {
             for (let offset = 0; offset < clientData.byteLength; offset += BUFFER_SIZE) {
-                const chunk = clientData.subarray(offset, offset + BUFFER_SIZE);
+                const chunk = clientData.slice(offset, offset + BUFFER_SIZE);
                 await writer.write(chunk);
             }
         } finally {
@@ -135,7 +135,7 @@ function makeWebStream(webSocket, earlyHeader) {
                 if (message instanceof ArrayBuffer || message instanceof Uint8Array) {
                     for (let offset = 0; offset < message.byteLength; offset += BUFFER_SIZE) {
                         const chunk = new Uint8Array(
-                            message.subarray(offset, offset + BUFFER_SIZE)
+                            message.slice(offset, offset + BUFFER_SIZE)
                         );
                         controller.enqueue(chunk);
                     }
