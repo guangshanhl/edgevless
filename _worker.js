c098b89c6ffe081691a6344f2d44bd69ca89b2ca
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-11-30 20:29:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -37,7 +37,6 @@ async function ressOverWSHandler(request) {
     const webSocketPair = new WebSocketPair();
     const [client, webSocket] = Object.values(webSocketPair);
     webSocket.accept();
-    let address = '';
     const earlyHeader = request.headers.get('sec-websocket-protocol') || '';
     const readableWebStream = makeWebStream(webSocket, earlyHeader);
     let remoteSocket = { value: null };
@@ -62,7 +61,6 @@ async function ressOverWSHandler(request) {
                 ressVersion = new Uint8Array([0, 0]),
                 isUDP,
             } = processRessHeader(chunk, userID);
-            address = addressRemote;
             if (hasError) {
                 return;
             }
