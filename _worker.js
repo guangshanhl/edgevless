6e098e585260b5c30b1bf3090ab73c7ed9f91d09
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-12-20 15:56:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -255,7 +255,8 @@ async function writeToSocket(socket, data) {
 function chunkData(data, size) {
     const chunks = [];
     for (let offset = 0; offset < data.byteLength; offset += size) {
-        chunks.push(new Uint8Array(data.slice(offset, offset + size)));
+        const end = Math.min(offset + size, data.byteLength);
+        chunks.push(data.subarray(offset, end));
     }
     return chunks;
 }
