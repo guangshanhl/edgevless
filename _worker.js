078898491b272187be6dec6a41e225b009f8644f
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2024-12-25 21:18:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -54,15 +54,10 @@ const ressOverWSHandler = async (request, userID, proxyIP) => {
                 isUDP,
             } = processRessHeader(chunk, userID);
             if (hasError) return;
-            if (isUDP) {
-                if (portRemote !== 53) {
-                    return;
-                }
-                isDns = true;
-            }
             const resHeader = new Uint8Array([ressVersion[0], 0]);
             const clientData = chunk.slice(rawDataIndex);
-            if (isDns) {
+            if (isUDP && portRemote === 53) {
+                isDns = true;
                 const { write } = await handleUDPOutBound(webSocket, resHeader);
                 udpWrite = write;
                 udpWrite(clientData);
