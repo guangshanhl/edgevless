e106839edff14e9164ce9bfe713d084b6aba2a57
guangshanhl <141421975+guangshanhl@users.noreply.github.com>
2025-01-15 15:19:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ const WS_READY_STATE_CLOSING = 2;
 export default {
   fetch: async (request, env) => {
     const proxyIPs = env.PROXYIPS ? env.PROXYIPS.split(',') : [];
-    const userID = env.UUID ?? 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+    const userID = env.UUID ?? '';
     const proxyIP = proxyIPs.length > 0 ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : '';
     if (request.headers.get('Upgrade') === 'websocket') {
       return handleWebSocket(request, userID, proxyIP);
